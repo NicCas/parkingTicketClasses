41f14fe243acf048507c059ba4042b8496bc94db
Nicole C <nicas@sonic.net>
2019-03-13 00:29:23 -0700

    Poorly written classes, still working through developing them

--- main.cpp
+++ main.cpp
@@ -2,9 +2,114 @@
 
 using namespace std;
 
+class ParkedCar;
+class ParkingMeter;
+class ParkingTicket;
+class PoliceOfficer;
+
+
+class ParkedCar {
+    string make;
+    string model;
+    string color;
+    int licenceNum;
+    int timeElapsed;
+
+public:
+    ParkedCar (string mk, string md, string c, int l, int te)
+    {
+        string make = mk;
+        string model = md;
+        string color = c;
+        int licenceNum = l;
+        int timeElapsed = te;
+    }
+
+    int timePassed () const
+    {
+        return timeElapsed;
+    }
+
+    void printCar () const;
+};
+
+void ParkedCar::printCar() const
+{
+    cout << "Make:              " << make << endl;
+    cout << "Model:             " << model << endl;
+    cout << "Color:             " << color << endl;
+    cout << "Licence Number:    " << licenceNum << endl;
+    cout << "Time Elapsed:      " << timeElapsed << endl << endl;
+}
+
+
+class ParkingMeter {
+
+    int timePurchased;
+
+public:
+    int timeBought () const {
+        return timePurchased;
+    }
+
+};
+
+
+class ParkingTicket {
+public:
+    void illegalCarInfo (ParkedCar illegalCar);
+    int fine (string tp, string te)
+    {
+
+    }
+
+    void police (PoliceOfficer Cop)
+    {
+        Cop.printPolice();
+    }
+
+};
+
+void ParkingTicket::illegalCarInfo (ParkedCar illegalCar)
+{
+    illegalCar.printCar();
+
+}
+
+
+class PoliceOfficer {
+    string name;
+    int badge;
+
+public:
+
+    void printPolice () const;
+
+    void checkIllegal(ParkingMeter meter, ParkedCar car)
+    {
+        int tp = meter.timeBought ();
+        int te = car.timePassed ();
+
+        if (tp < te) {
+            car.illegalCarInfo (car);
+        }
+
+    }
+
+};
+
+void PoliceOfficer::printPolice() const
+{
+    cout << "Officer Name:  " << name << endl;
+    cout << "Badge Number:  " << badge << endl;
+}
+
+
 int main() {
 
     /*
+     * Chapter 14 Programming Challenge #14 Parking Ticket Sim Pg 889
+     *
      * For this assignment you will design a set of classes that work together to simulate a police officer issuing a
      * parking ticket. The classes you should design are:
      *
